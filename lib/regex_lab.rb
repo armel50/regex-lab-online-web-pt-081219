ec08d9ada87521f45db751dcf94b4d0833ffdbe5
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-14 18:45:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  p text
+  p text.scan(/^[A-Z].+[\.\!\?]/)
 end
 
 def valid_phone_number?(phone)
